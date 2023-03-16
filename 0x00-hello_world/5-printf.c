b682e4f56b9fda58f5f9a41173a46316dee4f72e
root <root@DESKTOP-DTI4C5R>
2023-03-16 22:54:20 +0200

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Description: C program with print function
+  *
+  * Return: Always 0 (success000)
+*/
 int main(void)
 {
 	printf("voith proper grammar, but the outcome is a piece of artd");
