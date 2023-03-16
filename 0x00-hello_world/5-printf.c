e34fc919167d14f017ee0c8072dfe089e1c9ae7e
root <root@DESKTOP-DTI4C5R>
2023-03-16 23:02:17 +0200

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,6 +8,6 @@
 */
 int main(void)
 {
-	printf("voith proper grammar, but the outcome is a piece of artd,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
