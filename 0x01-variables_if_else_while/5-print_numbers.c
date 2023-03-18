a3d365b75269189f00ce2717daa3d93d62bf66c4
root <root@DESKTOP-DTI4C5R>
2023-03-18 02:10:34 +0200

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A C program that prints with printf function.
+ * Return: Always 0 (success000)
+*/
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		printf("%i", n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
